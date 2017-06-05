4c6265366cff65a9d3c2bf6171fd6be78af39948
Basile Koko <basile_koko@hotmail.com>
2017-06-05 21:17:00 +0100

    logscontroller action added

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,8 +1,40 @@
 class Api::V1::LogsController < ApplicationController
+  include Response
+
+  before_action :find_log, only: [:show, :update, :destroy]
 
   def index
     @logs = Log.all
-    render json: @logs
+    json_response(@logs)
+  end
+
+  def show
+    json_response(@log)
+  end
+
+  def create
+    @log = Log.create!(logs_params)
+    json_response(@log, :created)
+  end
+
+  def update
+    @log.update!(logs_params)
+    head :no_content
+  end
+
+  def destroy
+    @log.destroy!
+    head :no_content
+  end
+
+  private
+
+  def find_log
+    @log = Log.find(params[:id])
+  end
+
+  def logs_params
+    params.require(:log).permit(:what, :when, :where, :done)
   end
 
 end
